7824b1118d58f5ed154cd1a3425ee301cb8bde88
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-08-19 10:57:51 -0300

    feat(main): implements secure routes with react-auth-kit

--- ExitusWebApp/src/main.tsx
+++ ExitusWebApp/src/main.tsx
@@ -1,49 +1,66 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
-import LoginPage from './components/pages/LoginPage.tsx';
-import RegistersPage from './components/pages/RegistersPage.tsx';
-import StudentsPage from './components/pages/StudentsPage.tsx';
-import ResponsiblesPage from './components/pages/ResponsiblesPage.tsx';
-import LeavePage from './components/pages/LeavePage.tsx';
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom"
+import "./index.css"
+import LoginPage from './components/pages/LoginPage.tsx'
+import RegistersPage from './components/pages/RegistersPage.tsx'
+import StudentsPage from './components/pages/StudentsPage.tsx'
+import ResponsiblesPage from './components/pages/ResponsiblesPage.tsx'
+import LeavePage from './components/pages/LeavePage.tsx'
+import { RequireAuth, AuthProvider } from 'react-auth-kit'
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LoginPage />,
-    children: [
-      {
+    {
         path: "/login",
         element: <LoginPage />,
-      },
-      {
+    },
+    {
         path: "/register",
-        element: <RegistersPage />,
-      },
-      {
+        element: (
+            <RequireAuth loginPath='/login'>
+                <RegistersPage />
+            </RequireAuth>
+        ),
+    },
+    {
         path: "/student",
-        element: <StudentsPage />,
-      },
-      {
+        element: (
+            <RequireAuth loginPath='/login'>
+                <StudentsPage />
+            </RequireAuth>
+        ),
+    },
+    {
         path: "/reponsible",
-        element: <ResponsiblesPage />,
-      },
-      {
+        element: (
+            <RequireAuth loginPath='/login'>
+                <ResponsiblesPage />
+            </RequireAuth>
+        ),
+    },
+    {
         path: "/leave",
-        element: <LeavePage />,
-      }
-    ]
-  },
-]);
+        element: (
+            <RequireAuth loginPath='/login'>
+                <LeavePage />
+            </RequireAuth>
+        ),
+    }
+])
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <AuthProvider
+            authType={'cookie'}
+            authName={'_auth'}
+            cookieDomain={window.location.hostname}
+            cookieSecure={false}
+        >
+            <RouterProvider router={router} />
+        </AuthProvider>
+    </React.StrictMode>,
 )
