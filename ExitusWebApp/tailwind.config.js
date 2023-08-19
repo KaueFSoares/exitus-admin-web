30898695eb8d4965e824030d1c2157f6d65767eb
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-08-19 00:50:03 -0300

    feat(tailwind.config): sets the color palete on tailwind config
    * colors:
            'white': '#ffffff',
            'dark-green': '#00420C',
            'light-green': '#17882C',
            'red': '#C90C0F',

--- ExitusWebApp/tailwind.config.js
+++ ExitusWebApp/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white': '#ffffff',
+        'dark-green': '#00420C',
+        'light-green': '#17882C',
+        'red': '#C90C0F',
+      }
+    },
   },
   plugins: [],
 }
