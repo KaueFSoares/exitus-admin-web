b8003d831537e4efe520e40f24162efa5d3d1f18
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-08-21 01:41:26 -0300

    refactor: moves the context directory out of components directory

--- ExitusWebApp/src/context/NavbarContext.ts
+++ ExitusWebApp/src/context/NavbarContext.ts
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import { NavbarSelected } from "../../types/NavbarSelected.ts";
+import { NavbarSelected } from "../types/NavbarSelected.ts";
 
 const defaultNavbarValue: NavbarSelected = "register"
 
