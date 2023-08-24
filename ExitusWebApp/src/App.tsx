73960e51fa63a3b4de5e715f407852233ac255e8
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-08-24 20:20:09 -0300

    refactor: makes the App.tsx for implements translatable routes

---
+++ ExitusWebApp/src/App.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Home, LoginPage, RegistersPage, StudentsPage, ResponsiblesPage, LeavePage, NotFound } from './components/pages'
+import { AuthProvider, RequireAuth } from 'react-auth-kit'
+import { useTranslation } from 'react-i18next'
+import AppWrapper from './components/layout/wrapper/AppWrapper.tsx'
+
+
+
+
+const App = () => {
+
+    const { t } = useTranslation()
+
+    const router = createBrowserRouter([
+        {
+            path: t('url.home'),
+            element: <Home />,
+        },
+        {
+            path: t('url.login'),
+            element: <LoginPage />,
+        },
+        {
+            path: t('url.register'),
+            element: (
+                <RequireAuth loginPath={t('url.login')}>
+                    <RegistersPage />
+                </RequireAuth>
+            ),
+        },
+        {
+            path: t('url.student'),
+            element: (
+                <RequireAuth loginPath={t('url.login')}>
+                    <StudentsPage />
+                </RequireAuth>
+            ),
+        },
+        {
+            path: t('url.responsible'),
+            element: (
+                <RequireAuth loginPath={t('url.login')}>
+                    <ResponsiblesPage />
+                </RequireAuth>
+            ),
+        },
+        {
+            path: t('url.leave'),
+            element: (
+                <RequireAuth loginPath={t('url.login')}>
+                    <LeavePage />
+                </RequireAuth>
+            ),
+        },
+        {
+            path: "*",
+            element: <NotFound />
+        }
+    ])
+
+    return (
+        <AuthProvider
+            authType={'cookie'}
+            authName={'_auth'}
+            cookieDomain={window.location.hostname}
+            cookieSecure={false}
+        >
+
+            <AppWrapper>
+                <RouterProvider router={router} />
+            </AppWrapper>
+
+        </AuthProvider>
+    )
+}
+
+export default App
--- ExitusWebApp/src/main.tsx
+++ ExitusWebApp/src/main.tsx
@@ -1,86 +1,18 @@
 import React from 'react'
 
 import ReactDOM from 'react-dom/client'
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom"
 
 import "./index.css"
 import "./reset.css"
 
-import { RequireAuth, AuthProvider } from 'react-auth-kit'
-
-import AppWrapper from './components/layout/wrapper/AppWrapper.tsx'
-
-import {Home, LoginPage, RegistersPage, StudentsPage, ResponsiblesPage, LeavePage, NotFound} from './components/pages'
-
 import "./i18n"
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-    },
-    {
-        path: "/login",
-        element: <LoginPage />,
-    },
-    {
-        path: "/register",
-        element: (
-            <RequireAuth loginPath={"/login"}>
-                <RegistersPage />
-            </RequireAuth>
-        ),
-    },
-    {
-        path: "/student",
-        element: (
-            <RequireAuth loginPath={"/login"}>
-                <StudentsPage />
-            </RequireAuth>
-        ),
-    },
-    {
-        path: "/responsible",
-        element: (
-            <RequireAuth loginPath={"/login"}>
-                <ResponsiblesPage />
-            </RequireAuth>
-        ),
-    },
-    {
-        path: "/leave",
-        element: (
-            <RequireAuth loginPath={"/login"}>
-                <LeavePage />
-            </RequireAuth>
-        ),
-    },
-    {
-        path: "*",
-        element: <NotFound />
-    }
-])
-
-
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
     <React.StrictMode>
-        <AuthProvider
-            authType={'cookie'}
-            authName={'_auth'}
-            cookieDomain={window.location.hostname}
-            cookieSecure={false}
-        >
-
-            <AppWrapper>
-                <RouterProvider router={router} />
-            </AppWrapper>
-
-        </AuthProvider>
+        <App />
     </React.StrictMode>,
     
 )
